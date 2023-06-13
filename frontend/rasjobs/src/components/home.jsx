4d10d265d08383cade50f9553fb816ebd9b84dc2
surafel <picolo.sur@gmail.com>
2023-06-13 03:16:14 -0700

    create profile - lost more to be done

--- frontend/rasjobs/src/components/home.jsx
+++ frontend/rasjobs/src/components/home.jsx
@@ -1,19 +1,24 @@
-import React from 'react';
-
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom';
 import '../style/style.scss';
 import Profile from './profile';
 import Search from './search';
 import JobList from './jobList';
 import JobCard from './jobCard';
 import StatCard from './statCard';
-import { useLoaderData } from 'react-router-dom';
+import CreateProfile from './createProfile';
 
 export default function Home(props) {
 	const data = useLoaderData();
-
+	const [isLoged, setIsLgged] = useState(false);
 	return (
 		<div className='home'>
-			<Profile className='profile' />
+			{isLoged ? (
+				<Profile className='profile' />
+			) : (
+				<CreateProfile className='create-profile' />
+			)}
+
 			<div className='main'>
 				<Search className='search' />
 				<JobList className='job-list'>
