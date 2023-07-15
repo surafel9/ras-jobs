3b29e3e6923486e596e6444f3943518c3fdd5a8f
Surafel Kassa <96924000+surafel9@users.noreply.github.com>
2023-07-14 17:35:01 -0700

    Delete router directory

--- router/home.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const homeController = require('../controller/home');
-const homeRouter = express.Router();
-
-homeRouter.get('/', homeController);
-
-module.exports = homeRouter;
