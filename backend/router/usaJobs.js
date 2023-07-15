c91c10cdad58e9d676e28727ab4d9536ca09de2f
Surafel Kassa <96924000+surafel9@users.noreply.github.com>
2023-07-14 17:37:19 -0700

    Delete backend directory

--- backend/router/usaJobs.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const getUSAJOBS = require('../controller/usaJobs');
-
-const usaJobsRouter = express.Router();
-
-usaJobsRouter.get('/', getUSAJOBS);
-module.exports = usaJobsRouter;
